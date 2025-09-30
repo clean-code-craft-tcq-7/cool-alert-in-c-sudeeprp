31d372d00ad778d0fcdc62d02f3091906b7d33b2
Sudeep <sudeep.rp@gmail.com>
2025-09-30 15:15:43 +0530

    passive cooling test fail

--- test_cooling_alert.cpp
+++ test_cooling_alert.cpp
@@ -9,16 +9,16 @@ extern "C" {
 //   - Input parameters data-model
 //   - Expected output data-model
 
-// TEST(CoolingAlertTest, PassiveCoolingAlertToEmail) {
-//     Action action = battery_temperature_to_action(PASSIVE_COOLING, 45.0f);
-//     ASSERT_EQ(action.actionType, ALERT_EMAIL);
-//     ASSERT_STREQ(action.actionBody, "Temperature alert: 45.0F");
-// }
+TEST(CoolingAlertTest, PassiveCoolingAlertToEmail) {
+    Action action = battery_temperature_to_action(PASSIVE_COOLING, 45.0f);
+    ASSERT_EQ(action.actionType, ALERT_EMAIL);
+    ASSERT_STREQ(action.actionBody, "Temperature alert: 45.0F");
+}
 
-// TEST(CoolingAlertTest, NoAlert) {
-//     Action action = battery_temperature_to_action(PASSIVE_COOLING, 32.0f);
-//     ASSERT_EQ(action.actionType, NO_ALERT);
-// }
+TEST(CoolingAlertTest, NoAlert) {
+    Action action = battery_temperature_to_action(PASSIVE_COOLING, 32.0f);
+    ASSERT_EQ(action.actionType, NO_ALERT);
+}
 
 // ----------- STEP 2 code ends -------
 
