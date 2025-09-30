b754b013b0a81b786c398ead9bd8f1c060cd6da7
Sudeep <sudeep.rp@gmail.com>
2025-09-30 15:40:32 +0530

    passing thresholds and classification

--- classify.c
+++ classify.c
@@ -4,13 +4,13 @@
 // ----------- STEP 5 -----------------
 // Separation of classification from the email action.
 
-// BatteryState classify_battery_state(BatteryDataModel batteryData) {
-//     BatteryState state = BATTERY_OK;
-//     float alertThreshold = alertTemperatureForCoolingType(batteryData.thermalManagementType);
-//     if (batteryData.temperature >= alertThreshold) {
-//         state = BATTERY_ALERT;
-//     }
-//     return state;
-// }
+BatteryState classify_battery_state(BatteryDataModel batteryData) {
+    BatteryState state = BATTERY_OK;
+    float alertThreshold = alertTemperatureForCoolingType(batteryData.thermalManagementType);
+    if (batteryData.temperature >= alertThreshold) {
+        state = BATTERY_ALERT;
+    }
+    return state;
+}
 
 // ----------- STEP 5 code ends -------
