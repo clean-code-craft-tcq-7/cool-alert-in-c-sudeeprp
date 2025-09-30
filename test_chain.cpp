dbbf099c31fc7cb535fbccf0f280ff30a4572297
Sudeep <sudeep.rp@gmail.com>
2025-09-30 15:45:53 +0530

    failing chain

--- test_chain.cpp
+++ test_chain.cpp
@@ -5,32 +5,32 @@ extern "C" {
 
 // ----------- STEP 6 code starts -------
 
-// const char* email_to = 0;
-// const char* email_subject = 0;
-// const char* email_body = 0;
-// const char* email_from = 0;
+const char* email_to = 0;
+const char* email_subject = 0;
+const char* email_body = 0;
+const char* email_from = 0;
 
-// int emailSenderStub(const char *to, const char *subject,
-//                         const char *body, const char *from) {
-//     email_to = to;
-//     email_subject = subject;
-//     email_body = body;
-//     email_from = from;
-//     return 1; // Success
-// }
+int emailSenderStub(const char *to, const char *subject,
+                        const char *body, const char *from) {
+    email_to = to;
+    email_subject = subject;
+    email_body = body;
+    email_from = from;
+    return 1; // Success
+}
 
-// Actuators actuatorStubSet() {
-//     Actuators actuators;
-//     actuators.emailSender = emailSenderStub;
-//     return actuators;
-// }
+Actuators actuatorStubSet() {
+    Actuators actuators;
+    actuators.emailSender = emailSenderStub;
+    return actuators;
+}
 
-// TEST(ChainTest, BatteryDataToAction) {
-//     BatteryDataModel batteryData = {10342, THERMAL_HYBRID, 60};
+TEST(ChainTest, BatteryDataToAction) {
+    BatteryDataModel batteryData = {10342, THERMAL_HYBRID, 60};
 
-//     batteryDataToAction(batteryData, actuatorStubSet);
+    batteryDataToAction(batteryData, actuatorStubSet);
 
-//     ASSERT_STREQ(email_to, "manager@battery.com");
-// }
+    ASSERT_STREQ(email_to, "manager@battery.com");
+}
 
 // ----------- STEP 6 code ends -------
